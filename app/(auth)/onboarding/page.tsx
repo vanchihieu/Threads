45b4f5bb0d4a4b8b00b9c9283c1200f389fb34a1
Van Chi Hieu <vanchihieu3@gmail.com>
2023-10-03 16:09:35 +0700

    Home page

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const page = () => {
+    return (
+        <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+            <h1 className="head-text">Onboarding</h1>
+            <p className="mt-3 text-base-regular text-light-2">
+                Complete your profile now, to use Threds.
+            </p>
+
+           
+        </main>
+    );
+};
+
+export default page;
