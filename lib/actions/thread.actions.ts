c57fd497558b23d73576add2b515df5394bd8755
Van Chi Hieu <vanchihieu3@gmail.com>
2023-10-05 19:55:36 +0700

    fetchPost Homepage

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -5,43 +5,87 @@ import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 
-interface Params {
-    text: string,
-    author: string,
-    communityId: string | null,
-    path: string,
-  }
-  
-  export async function createThread({ text, author, communityId, path }: Params
-  ) {
-    try {
-      connectToDB();
-  
-      const communityIdObject = await Community.findOne(
-        { id: communityId },
-        { _id: 1 }
-      );
-  
-      const createdThread = await Thread.create({
-        text,
-        author,
-        community: communityIdObject, // Assign communityId if provided, or leave it null for personal account
-      });
-  
-      // Update User model
-      await User.findByIdAndUpdate(author, {
-        $push: { threads: createdThread._id },
-      });
-  
-      if (communityIdObject) {
-        // Update Community model
-        await Community.findByIdAndUpdate(communityIdObject, {
-          $push: { threads: createdThread._id },
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    connectToDB();
+
+    // Calculate the number of posts to skip based on the page number and page size.
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    // Create a query to fetch the posts that have no parent (top-level threads) (a thread that is not a comment/reply).
+    const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+        .sort({ createdAt: "desc" })
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({
+            path: "author",
+            model: User,
+        })
+        // .populate({
+        //     path: "community",
+        //     model: Community,
+        // })
+        .populate({
+            path: "children", // Populate the children field
+            populate: {
+                path: "author", // Populate the author field within children
+                model: User,
+                select: "_id name parentId image", // Select only _id and username fields of the author
+            },
         });
-      }
-  
-      revalidatePath(path);
+
+    // Count the total number of top-level posts (threads) i.e., threads that are not comments.
+    const totalPostsCount = await Thread.countDocuments({
+        parentId: { $in: [null, undefined] },
+    }); // Get the total count of posts
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext };
+}
+
+interface Params {
+    text: string;
+    author: string;
+    communityId: string | null;
+    path: string;
+}
+
+export async function createThread({
+    text,
+    author,
+    communityId,
+    path,
+}: Params) {
+    try {
+        connectToDB();
+
+        const communityIdObject = await Community.findOne(
+            { id: communityId },
+            { _id: 1 }
+        );
+
+        const createdThread = await Thread.create({
+            text,
+            author,
+            community: communityIdObject, // Assign communityId if provided, or leave it null for personal account
+        });
+
+        // Update User model
+        await User.findByIdAndUpdate(author, {
+            $push: { threads: createdThread._id },
+        });
+
+        if (communityIdObject) {
+            // Update Community model
+            await Community.findByIdAndUpdate(communityIdObject, {
+                $push: { threads: createdThread._id },
+            });
+        }
+
+        revalidatePath(path);
     } catch (error: any) {
-      throw new Error(`Failed to create thread: ${error.message}`);
+        throw new Error(`Failed to create thread: ${error.message}`);
     }
-  }
+}
